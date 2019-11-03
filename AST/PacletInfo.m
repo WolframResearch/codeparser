0d6834888dcfda0ef90bffd1766f16d6ceaa2f84
Brenton Bostick <brenton@wolfram.com>
2019-11-03 17:44:49 -0600

    Bump to 0.15

--- AST/PacletInfo.m
+++ AST/PacletInfo.m
@@ -1,7 +1,7 @@
 
 Paclet[
     Name -> "AST",
-    Version -> "0.14",
+    Version -> "0.15",
     WolframVersion -> "11.0+",
     Extensions -> {
     		{"Kernel", Root -> "Kernel", Context -> "AST`"},
