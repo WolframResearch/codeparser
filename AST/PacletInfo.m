1f530583ceb6401670d66ec5800b5828a5e74035
Brenton Bostick <brenton@wolfram.com>
2019-11-07 09:31:06 -0600

    Add Creator field

--- AST/PacletInfo.m
+++ AST/PacletInfo.m
@@ -3,6 +3,7 @@ Paclet[
     Name -> "AST",
     Version -> "0.15",
     WolframVersion -> "11.0+",
+    Creator ->"Brenton Bostick <brenton@wolfram.com>",
     Extensions -> {
     		{"Kernel", Root -> "Kernel", Context -> "AST`"},
     		{"Documentation", Language -> "English"},
