d121ad69b22e10a3ad664a7120c471823ffc6e02
Brenton Bostick <brenton@wolfram.com>
2020-01-16 11:17:30 -0600

    Work-around bug in WolframScript exiting with non-0 exit code

--- AST/Generate/PackPaclet.wl
+++ AST/Generate/PackPaclet.wl
@@ -35,13 +35,3 @@ Print["Done"]
 End[]
 
 EndPackage[]
-
-
-(*
-Needed to guarantee exit code of 0 in older versions
-
-Seen with v11.1 on Windows if not calling Quit[0]:
-error MSB6006: "cmd.exe" exited with code -1073741819. [C:\Jenkins\workspace\Component.AST.Windows-x86-64.prototype\ast\build\paclet.vcxproj]
-Related bugs: ?
-*)
-Quit[0]
---
+++ cmake/PackPaclet.cmake
@@ -0,0 +1,24 @@
+
+#
+# Setup a script to work-around non-0 exit codes when running WolframScript
+#
+# Seen with v11.1 on Windows
+# error MSB6006: "cmd.exe" exited with code -1073741819. [C:\Jenkins\workspace\Component.AST.Windows-x86-64.prototype\ast\build\paclet.vcxproj]
+#
+# The -1073741819 is really just 0xc0000005 and means a crash happened.
+#
+# This might be some strange confluence of Windows, WolframScript, and Java
+#
+# Related bugs: ?
+#
+
+execute_process(
+  COMMAND
+    ${WOLFRAMKERNEL} -noinit -script ${PACKPACLET_WL_SCRIPT} -pacletDir ${PACLETDIR}
+  RESULT_VARIABLE
+    PACKPACLET_RESULT
+)
+
+if(NOT ${PACKPACLET_RESULT} EQUAL "0")
+  message(WARNING "Bad exit code from PackPaclet script: ${PACKPACLET_RESULT}; Continuing")
+endif()
