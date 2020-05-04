4067724eee1ab1e5c74218355f3a1a894daa3409
Brenton Bostick <brenton@wolfram.com>
2020-05-04 06:56:59 -0500

    Update building of PacletInfo.m
    Instead of replacing text inside of a PacletInfo.m file, have an entire file that needs to be copied, while replacing text in the process.

--- cmake/ReplacePacletInfo.cmake
+++ cmake/ReplacePacletInfo.cmake
@@ -1,8 +1,6 @@
 
 file(READ ${COPIED_PACLETINFO} filedata)
 
-string(REGEX REPLACE "UnbuiltPaclet" "Paclet" filedata ${filedata})
-
 if(LOCAL_BUILD)
 
 string(REGEX REPLACE "Version -> \"[0-9\\.]+\"" "Version -> \"999\"(* local build *)" filedata ${filedata})
