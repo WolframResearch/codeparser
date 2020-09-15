1308380ab7be3b1a93ec6262fd4ff5c2f7167128
Brenton Bostick <brenton@wolfram.com>
2020-09-15 12:23:25 -0500

    Add comment at build time containing build date

--- cmake/ReplacePacletInfo.cmake
+++ cmake/ReplacePacletInfo.cmake
@@ -1,6 +1,10 @@
 
 file(READ ${COPIED_PACLETINFO} filedata)
 
+string(TIMESTAMP DATESTRING_COMMENT "(* Paclet built: %a %d %b %Y %H:%M:%S *)\n")
+
+string(PREPEND filedata ${DATESTRING_COMMENT})
+
 if(LOCAL_BUILD)
 
 string(REGEX REPLACE "Version -> \"[0-9\\.]+\"" "Version -> \"999\"(* local build *)" filedata ${filedata})
