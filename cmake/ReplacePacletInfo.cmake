d678fab213f4ffeb09578923435d5a628a5b5d37
Brenton Bostick <brenton@wolfram.com>
2020-12-14 12:33:49 -0600

    Properly handle replacing "Documentation" extension in local build

--- cmake/ReplacePacletInfo.cmake
+++ cmake/ReplacePacletInfo.cmake
@@ -9,7 +9,10 @@ if(LOCAL_BUILD)
 
 string(REGEX REPLACE "Version -> \"[0-9\\.]+\"" "Version -> \"${LOCAL_BUILD_VERSION}\"(* local build *)" filedata ${filedata})
 
-string(REGEX REPLACE "(\\{\"Documentation\"[a-zA-Z0-9 ->\"/]*\\},)" "(* no \"Documentation\" in local build \\1 *)" filedata ${filedata})
+# try replacing {"Documentation"} extension that is not first in list
+string(REGEX REPLACE ",\n([ \t]*)(\\{\"Documentation\"[a-zA-Z0-9 ->\"/]*\\})" "\n\\1(* no \"Documentation\" in local build \\2 *)" filedata ${filedata})
+# now try replacing {"Documentation"} extension that IS first in list
+string(REGEX REPLACE "(\\{\"Documentation\"[a-zA-Z0-9 ->\"/]*\\})" "(* no \"Documentation\" in local build \\1 *)" filedata ${filedata})
 
 endif()
 
