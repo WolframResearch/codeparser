e3f0aef7cea91140cb3c53413d1b021f3ecccb11
Brenton Bostick <brenton@wolfram.com>
2020-12-15 13:49:23 -0600

    Inspect PacletInfo.wl AFTER it has been replaced

--- cmake/ReplacePacletInfo.cmake
+++ cmake/ReplacePacletInfo.cmake
@@ -9,10 +9,11 @@ if(LOCAL_BUILD)
 
 string(REGEX REPLACE "Version -> \"[0-9\\.]+\"" "Version -> \"${LOCAL_BUILD_VERSION}\"(* local build *)" filedata ${filedata})
 
-# try replacing {"Documentation"} extension that is not first in list
-string(REGEX REPLACE ",\n([ \t]*)(\\{\"Documentation\"[a-zA-Z0-9 ->\"/]*\\})" "\n\\1(* no \"Documentation\" in local build \\2 *)" filedata ${filedata})
-# now try replacing {"Documentation"} extension that IS first in list
-string(REGEX REPLACE "(\\{\"Documentation\"[a-zA-Z0-9 ->\"/]*\\})" "(* no \"Documentation\" in local build \\1 *)" filedata ${filedata})
+#
+# Handle "Documentation" in list of extensions
+# Fails if "Documentation" extension is first in list of extensions
+#
+string(REGEX REPLACE ",\n([ \t]*)\\{\"Documentation\"[a-zA-Z0-9 ->\"/]*\\}(,|)" "\\2\n\\1(* no \"Documentation\" in local build *)" filedata ${filedata})
 
 endif()
 
