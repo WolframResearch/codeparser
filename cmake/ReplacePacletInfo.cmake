28129760b7f673779efa1dbaeef2a5247832beea
Brenton Bostick <brenton@wolfram.com>
2021-03-25 08:57:22 -0500

    Move the replacing of PacletInfo content to the paclet build command

--- cmake/ReplacePacletInfo.cmake
+++ cmake/ReplacePacletInfo.cmake
@@ -3,11 +3,11 @@ file(READ ${COPIED_PACLETINFO} filedata)
 
 string(TIMESTAMP DATESTRING "%a %d %b %Y %H:%M:%S")
 
-string(REGEX REPLACE "BuildDate -> \"\"" "BuildDate -> \"${DATESTRING}\"" filedata ${filedata})
+string(REGEX REPLACE "BuildDate -> \"[a-zA-Z0-9 :]*\"" "BuildDate -> \"${DATESTRING}\"" filedata ${filedata})
 
 if(LOCAL_BUILD)
 
-string(REGEX REPLACE "Version -> \"[0-9\\.]+\"" "Version -> \"${LOCAL_BUILD_VERSION}\"(* local build *)" filedata ${filedata})
+string(REGEX REPLACE "Version -> \"[0-9\\.]+\"," "Version -> \"${LOCAL_BUILD_VERSION}\"(* local build *)," filedata ${filedata})
 
 #
 # Handle "Documentation" in list of extensions
