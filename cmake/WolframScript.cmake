5405006519a85cdf7f8c262114e1e7836fcd0bb9
Brenton Bostick <brenton@wolfram.com>
2022-02-25 10:27:09 -0600

    Add RETRY_ON_FAILURE variable to WolframScript.cmake
    Try to help with CreatePacletArchive being slow on RE machines,
    and hitting JLink connection timeout
    
    There are other work-arounds, but retrying is good because it allows
    the error to still be reported and possibly allow a proper fix in the
    future.

--- cmake/WolframScript.cmake
+++ cmake/WolframScript.cmake
@@ -3,6 +3,16 @@ if(NOT EXISTS ${WOLFRAMKERNEL})
 message(FATAL_ERROR "WOLFRAMKERNEL does not exist. WOLFRAMKERNEL: ${WOLFRAMKERNEL}")
 endif()
 
+if(NOT DEFINED RETRY_ON_FAILURE)
+set(RETRY_ON_FAILURE OFF)
+endif()
+
+if(RETRY_ON_FAILURE)
+
+#
+# try twice
+#
+
 execute_process(
   COMMAND
     ${WOLFRAMKERNEL} -script ${SCRIPT} -srcDir ${SRCDIR} -buildDir ${BUILDDIR} -paclet ${PACLET}
@@ -13,5 +23,40 @@ execute_process(
 )
 
 if(NOT ${SCRIPT_RESULT} EQUAL "0")
-  message(FATAL_ERROR "Bad exit code from script: ${SCRIPT_RESULT}")
+message(WARNING "First try: Bad exit code from script: ${SCRIPT_RESULT}; retrying...")
+
+execute_process(
+  COMMAND
+    ${WOLFRAMKERNEL} -script ${SCRIPT} -srcDir ${SRCDIR} -buildDir ${BUILDDIR} -paclet ${PACLET}
+  TIMEOUT
+    ${BUG349779_TIMEOUT}
+  RESULT_VARIABLE
+    SCRIPT_RESULT
+)
+
+if(NOT ${SCRIPT_RESULT} EQUAL "0")
+message(FATAL_ERROR "Second try: Bad exit code from script: ${SCRIPT_RESULT}; stopping")
+endif()
+
+endif()
+
+else(RETRY_ON_FAILURE)
+
+#
+# only try once
+#
+
+execute_process(
+  COMMAND
+    ${WOLFRAMKERNEL} -script ${SCRIPT} -srcDir ${SRCDIR} -buildDir ${BUILDDIR} -paclet ${PACLET}
+  TIMEOUT
+    ${BUG349779_TIMEOUT}
+  RESULT_VARIABLE
+    SCRIPT_RESULT
+)
+
+if(NOT ${SCRIPT_RESULT} EQUAL "0")
+message(FATAL_ERROR "Bad exit code from script: ${SCRIPT_RESULT}")
+endif()
+
 endif()
