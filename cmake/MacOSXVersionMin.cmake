1121b906116a6cb013f6e5f7102e722da0c502f0
Brenton Bostick <brenton@wolfram.com>
2022-08-23 08:07:44 -0500

    Get MacOSXVersionMin from CCompilerDriver and warn if using a different version

---
+++ cmake/MacOSXVersionMin.cmake
@@ -0,0 +1,29 @@
+
+macro(CheckMacOSXVersionMin)
+
+  if(NOT EXISTS ${WOLFRAMKERNEL})
+  message(FATAL_ERROR "WOLFRAMKERNEL does not exist. WOLFRAMKERNEL: ${WOLFRAMKERNEL}")
+  endif()
+
+  #
+  # if not local build, then get Version from PacletInfo.wl
+  #
+  execute_process(
+    COMMAND
+      ${WOLFRAMKERNEL} -noinit -noprompt -nopaclet -nostartuppaclets -run Pause[${KERNEL_PAUSE}]\;Needs["CCompilerDriver`"]\;Print[OutputForm[StringReplace[CCompilerDriver`CCompilerDriverBase`MacOSXVersionMinFlag[],\ "-mmacosx-version-min="\ ->\ ""]]]\;Exit[]
+    OUTPUT_VARIABLE
+      MACOSX_VERSION_MIN
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+    WORKING_DIRECTORY
+      ${PROJECT_SOURCE_DIR}
+    TIMEOUT
+      ${KERNEL_TIMEOUT}
+    RESULT_VARIABLE
+      MACOSX_VERSION_MIN_RESULT
+  )
+
+  if(NOT ${MACOSX_VERSION_MIN_RESULT} EQUAL "0")
+    message(FATAL_ERROR "Bad exit code from MacOSXVersionMin script: ${MACOSX_VERSION_MIN_RESULT}")
+  endif()
+
+endmacro(CheckMacOSXVersionMin)
