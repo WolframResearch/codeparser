7fa18f3014c0e392fa58d31242c7fab327437cb5
Connor Gray <connorg@wolfram.com>
2022-09-07 12:23:58 -0400

    feature: Include initial Rust benchmark code for large files
    The current benchmark timings for these files on my computer are:
    
            large files/tokenize ReliefPlot.nb
                    time:   [1.0504 s 1.0516 s 1.0528 s]
    
            large files/tokenize expandedCompanyDataNew1.m
                    time:   [885.92 ms 889.35 ms 894.00 ms]
    
            large files/tokenize geomagneticmodels.m
                    time:   [162.24 ms 165.91 ms 169.95 ms]

--- crate/benches/bench_general.rs
+++ crate/benches/bench_general.rs
@@ -1,13 +1,19 @@
-use criterion::{criterion_group, criterion_main, Criterion};
+use std::{fs, path::Path};
+
+use criterion::{criterion_group, criterion_main, Criterion, SamplingMode};
 
 use wolfram_code_parse::{
     EncodingMode, FirstLineBehavior, ParserSession, SourceConvention::LineColumn,
 };
 
 fn parse_tokens(input: &str) {
+    parse_tokens_u8(input.as_bytes())
+}
+
+fn parse_tokens_u8(input: &[u8]) {
     let tab_width = 4;
     let mut session = ParserSession::new(
-        input.as_bytes(),
+        input,
         LineColumn,
         tab_width,
         FirstLineBehavior::NotScript,
@@ -21,8 +27,43 @@ fn benchmark(c: &mut Criterion) {
     c.bench_function("tokenize 2 + 2", |b| b.iter(|| parse_tokens("2 + 2")));
 
     let boxes_wl = include_str!("../../CodeParser/Kernel/Boxes.wl");
-
     c.bench_function("tokenize Boxes.wl", |b| b.iter(|| parse_tokens(boxes_wl)));
+
+    benchmark_large_files(c);
+}
+
+fn benchmark_large_files(c: &mut Criterion) {
+    let mut group = c.benchmark_group("large files");
+    group.sampling_mode(SamplingMode::Flat);
+    group.sample_size(10);
+
+    //------------
+    // Large files
+    //------------
+
+    let relief_plot = fs::read(Path::new("../Tests/files/large/ReliefPlot.nb")).unwrap();
+    group.bench_function("tokenize ReliefPlot.nb", |b| {
+        b.iter(|| parse_tokens_u8(&relief_plot))
+    });
+
+    let expanded_company_data_new =
+        fs::read(Path::new("../Tests/files/large/expandedCompanyDataNew1.m")).unwrap();
+    group.bench_function("tokenize expandedCompanyDataNew1.m", |b| {
+        b.iter(|| parse_tokens_u8(&expanded_company_data_new))
+    });
+
+    //-------------
+    // Medium files
+    //-------------
+
+    group.sampling_mode(SamplingMode::Auto);
+    group.sample_size(30);
+
+    let geomagnetic_models =
+        fs::read(Path::new("../Tests/files/large/geomagneticmodels.m")).unwrap();
+    group.bench_function("tokenize geomagneticmodels.m", |b| {
+        b.iter(|| parse_tokens_u8(&geomagnetic_models))
+    });
 }
 
 criterion_group!(benches, benchmark);
