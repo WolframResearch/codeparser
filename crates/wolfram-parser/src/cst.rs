e1f5710aa1b5725fa0bd388bcb42603b1b72bb07
Connor Gray <connorg@wolfram.com>
2023-10-09 11:04:23 -0500

    cleanup: Consolidate visitor impls in new cst/visit.rs

--- crates/wolfram-parser/src/cst.rs
+++ crates/wolfram-parser/src/cst.rs
@@ -2,6 +2,8 @@
 //!
 //! [`Cst`] — root and element type in a concrete syntax tree.
 
+mod visit;
+
 use wolfram_expr::{symbol::SymbolRef, Expr};
 
 use crate::{
@@ -278,38 +280,6 @@ impl<I, S> From<CodeNode<S>> for Cst<I, S> {
 // NodeSeq
 //======================================
 
-impl<I, S> CstSeq<I, S> {
-    pub fn visit(&self, visit: &mut dyn FnMut(&Cst<I, S>)) {
-        let NodeSeq(elements) = self;
-
-        for elem in elements {
-            elem.visit(visit);
-        }
-    }
-
-    pub fn visit_mut(&mut self, visit: &mut dyn FnMut(&mut Cst<I, S>)) {
-        let NodeSeq(elements) = self;
-
-        for elem in elements {
-            elem.visit_mut(visit);
-        }
-    }
-
-    pub fn map_visit(
-        self,
-        visit: &mut dyn FnMut(Cst<I, S>) -> Cst<I, S>,
-    ) -> Self {
-        let NodeSeq(elements) = self;
-
-        let elements = elements
-            .into_iter()
-            .map(|elem| elem.map_visit(visit))
-            .collect();
-
-        NodeSeq(elements)
-    }
-}
-
 impl<N> NodeSeq<N> {
     pub fn push<I: Into<N>>(&mut self, node: I) {
         let NodeSeq(vec) = self;
@@ -383,192 +353,6 @@ impl<I: TokenInput, S> CstSeq<I, S> {
 //==========================================================
 // Nodes
 //==========================================================
-
-impl<I, S> Cst<I, S> {
-    /// Visit this node and every child node, recursively.
-    pub fn visit(&self, visit: &mut dyn FnMut(&Cst<I, S>)) {
-        // Visit the current node.
-        visit(self);
-
-        // Visit child nodes.
-        match self {
-            Cst::Token(_) => (),
-            Cst::Call(CallNode { head, body, src: _ }) => {
-                head.visit(visit);
-                body.as_op().visit_children(visit);
-            },
-            Cst::SyntaxError(SyntaxErrorNode {
-                err: _,
-                children,
-                src: _,
-            }) => {
-                children.visit(visit);
-            },
-            Cst::Prefix(PrefixNode(op)) => op.visit_children(visit),
-            Cst::Infix(InfixNode(op)) => op.visit_children(visit),
-            Cst::Postfix(PostfixNode(op)) => op.visit_children(visit),
-            Cst::Binary(BinaryNode(op)) => op.visit_children(visit),
-            Cst::Ternary(TernaryNode(op)) => op.visit_children(visit),
-            Cst::PrefixBinary(PrefixBinaryNode(op)) => op.visit_children(visit),
-            Cst::Compound(CompoundNode(op)) => op.visit_children(visit),
-            Cst::Group(GroupNode(op))
-            | Cst::GroupMissingCloser(GroupMissingCloserNode(op))
-            | Cst::GroupMissingOpener(GroupMissingOpenerNode(op)) => {
-                op.visit_children(visit)
-            },
-            Cst::Box(BoxNode {
-                kind: _,
-                children,
-                src: _,
-            }) => {
-                children.visit(visit);
-            },
-            // These node types have no child nodes.
-            Cst::Code(_) => (),
-        }
-    }
-
-    /// Mutably visit this node and every child node, recursively.
-    pub fn visit_mut(&mut self, visit: &mut dyn FnMut(&mut Cst<I, S>)) {
-        // Visit the current node.
-        visit(self);
-
-        // Visit child nodes.
-        match self {
-            Cst::Token(_) => (),
-            Cst::Call(CallNode { head, body, src: _ }) => {
-                head.visit_mut(visit);
-
-                body.as_op_mut().visit_children_mut(visit);
-            },
-            Cst::SyntaxError(SyntaxErrorNode {
-                err: _,
-                children,
-                src: _,
-            }) => children.visit_mut(visit),
-
-            Cst::Infix(InfixNode(op)) => {
-                op.visit_children_mut(visit);
-            },
-            Cst::Prefix(PrefixNode(op)) => {
-                op.visit_children_mut(visit);
-            },
-            Cst::Postfix(PostfixNode(op)) => {
-                op.visit_children_mut(visit);
-            },
-            Cst::Binary(BinaryNode(op)) => {
-                op.visit_children_mut(visit);
-            },
-            Cst::Ternary(TernaryNode(op)) => {
-                op.visit_children_mut(visit);
-            },
-            Cst::PrefixBinary(PrefixBinaryNode(op)) => {
-                op.visit_children_mut(visit)
-            },
-            Cst::Compound(CompoundNode(op)) => op.visit_children_mut(visit),
-            Cst::Group(GroupNode(op)) => op.visit_children_mut(visit),
-            Cst::GroupMissingCloser(GroupMissingCloserNode(op)) => {
-                op.visit_children_mut(visit)
-            },
-            Cst::GroupMissingOpener(GroupMissingOpenerNode(op)) => {
-                op.visit_children_mut(visit)
-            },
-
-            Cst::Box(BoxNode {
-                kind: _,
-                children,
-                src: _,
-            }) => {
-                children.visit_mut(visit);
-            },
-
-            // These node types have no child nodes.
-            Cst::Code(_) => (),
-        }
-    }
-
-    /// Transform this node tree by visiting this node and every child node, recursively.
-    pub fn map_visit(
-        self,
-        visit: &mut dyn FnMut(Cst<I, S>) -> Cst<I, S>,
-    ) -> Self {
-        // Visit the current node.
-        let self_ = visit(self);
-
-        // Visit child nodes.
-        let node: Cst<I, S> = match self_ {
-            Cst::Token(_) => return self_,
-            Cst::Call(CallNode { head, body, src }) => {
-                let head = head.map_visit(visit);
-
-                let body = body.map_op(|body_op: OperatorNode<_, _, _>| {
-                    body_op.map_visit(visit)
-                });
-
-                Cst::Call(CallNode { head, body, src })
-            },
-            Cst::SyntaxError(SyntaxErrorNode { err, children, src }) => {
-                let children = children.map_visit(visit);
-
-                Cst::SyntaxError(SyntaxErrorNode { err, children, src })
-            },
-
-            Cst::Infix(InfixNode(op)) => {
-                Cst::Infix(InfixNode(op.map_visit(visit)))
-            },
-            Cst::Prefix(PrefixNode(op)) => {
-                Cst::Prefix(PrefixNode(op.map_visit(visit)))
-            },
-            Cst::Postfix(PostfixNode(op)) => {
-                Cst::Postfix(PostfixNode(op.map_visit(visit)))
-            },
-            Cst::Binary(BinaryNode(op)) => {
-                Cst::Binary(BinaryNode(op.map_visit(visit)))
-            },
-            Cst::Ternary(TernaryNode(op)) => {
-                Cst::Ternary(TernaryNode(op.map_visit(visit)))
-            },
-            Cst::PrefixBinary(PrefixBinaryNode(op)) => {
-                Cst::PrefixBinary(PrefixBinaryNode(op.map_visit(visit)))
-            },
-            Cst::Compound(CompoundNode(op)) => {
-                Cst::Compound(CompoundNode(op.map_visit(visit)))
-            },
-            Cst::Group(GroupNode(op)) => {
-                Cst::Group(GroupNode(op.map_visit(visit)))
-            },
-            Cst::GroupMissingCloser(GroupMissingCloserNode(op)) => {
-                Cst::GroupMissingCloser(GroupMissingCloserNode(
-                    op.map_visit(visit),
-                ))
-            },
-            Cst::GroupMissingOpener(GroupMissingOpenerNode(op)) => {
-                Cst::GroupMissingOpener(GroupMissingOpenerNode(
-                    op.map_visit(visit),
-                ))
-            },
-
-            Cst::Box(BoxNode {
-                kind,
-                children,
-                src,
-            }) => {
-                let children = children.map_visit(visit);
-
-                Cst::Box(BoxNode {
-                    kind,
-                    children,
-                    src,
-                })
-            },
-
-            // These node types have no child nodes.
-            node @ Cst::Code(_) => node,
-        };
-
-        node
-    }
-}
 
 impl<I: TokenInput, S> Cst<I, S> {
     pub fn into_owned_input(self) -> Cst<TokenString, S> {
@@ -757,41 +541,6 @@ impl<I: TokenInput, S, O> OperatorNode<I, S, O> {
     }
 }
 
-impl<I, S, O> OperatorNode<I, S, O> {
-    /// Visit this node and every child node, recursively.
-    fn visit_children(&self, visit: &mut dyn FnMut(&Cst<I, S>)) {
-        let OperatorNode {
-            op: _,
-            children,
-            src: _,
-        } = self;
-
-        children.visit(visit);
-    }
-
-    /// Mutably visit this node and every child node, recursively.
-    fn visit_children_mut(&mut self, visit: &mut dyn FnMut(&mut Cst<I, S>)) {
-        let OperatorNode {
-            op: _,
-            children,
-            src: _,
-        } = self;
-
-        children.visit_mut(visit);
-    }
-
-    pub fn map_visit(
-        self,
-        visit: &mut dyn FnMut(Cst<I, S>) -> Cst<I, S>,
-    ) -> Self {
-        let OperatorNode { op, children, src } = self;
-
-        let children = children.map_visit(visit);
-
-        OperatorNode { op, children, src }
-    }
-}
-
 //======================================
 // Missing closer nodes
 //======================================
@@ -943,37 +692,6 @@ impl<I, S> CallNode<I, S> {
 impl<I, S> CallHead<I, S> {
     pub fn aggregate(node: Cst<I, S>) -> Self {
         CallHead::Aggregate(Box::new(node))
-    }
-
-    /// Visit this node and every child node, recursively.
-    pub fn visit(&self, visit: &mut dyn FnMut(&Cst<I, S>)) {
-        match self {
-            CallHead::Concrete(head) => head.visit(visit),
-            CallHead::Aggregate(head) => head.visit(visit),
-        }
-    }
-
-    /// Mutably visit this node and every child node, recursively.
-    pub fn visit_mut(&mut self, visit: &mut dyn FnMut(&mut Cst<I, S>)) {
-        match self {
-            CallHead::Concrete(head) => head.visit_mut(visit),
-            CallHead::Aggregate(head) => head.visit_mut(visit),
-        }
-    }
-
-    pub fn map_visit(
-        self,
-        visit: &mut dyn FnMut(Cst<I, S>) -> Cst<I, S>,
-    ) -> Self {
-        match self {
-            CallHead::Concrete(head) => {
-                CallHead::Concrete(head.map_visit(visit))
-            },
-            CallHead::Aggregate(head) => {
-                let head: Cst<I, S> = *head;
-                CallHead::Aggregate(Box::new(head.map_visit(visit)))
-            },
-        }
     }
 
     pub(crate) fn check(&self) -> bool {
---
+++ crates/wolfram-parser/src/cst/visit.rs
@@ -0,0 +1,313 @@
+//! Visitor methods for [`Cst`] and related types.
+
+use crate::{
+    cst::{
+        BinaryNode, BoxNode, CallHead, CallNode, CompoundNode, Cst, CstSeq,
+        GroupMissingCloserNode, GroupMissingOpenerNode, GroupNode, InfixNode,
+        OperatorNode, PostfixNode, PrefixBinaryNode, PrefixNode,
+        SyntaxErrorNode, TernaryNode,
+    },
+    NodeSeq,
+};
+
+//======================================
+// CstSeq
+//======================================
+
+impl<I, S> CstSeq<I, S> {
+    pub fn visit(&self, visit: &mut dyn FnMut(&Cst<I, S>)) {
+        let NodeSeq(elements) = self;
+
+        for elem in elements {
+            elem.visit(visit);
+        }
+    }
+
+    pub fn visit_mut(&mut self, visit: &mut dyn FnMut(&mut Cst<I, S>)) {
+        let NodeSeq(elements) = self;
+
+        for elem in elements {
+            elem.visit_mut(visit);
+        }
+    }
+
+    pub fn map_visit(
+        self,
+        visit: &mut dyn FnMut(Cst<I, S>) -> Cst<I, S>,
+    ) -> Self {
+        let NodeSeq(elements) = self;
+
+        let elements = elements
+            .into_iter()
+            .map(|elem| elem.map_visit(visit))
+            .collect();
+
+        NodeSeq(elements)
+    }
+}
+
+//======================================
+// Cst
+//======================================
+
+impl<I, S> Cst<I, S> {
+    /// Visit this node and every child node, recursively.
+    pub fn visit(&self, visit: &mut dyn FnMut(&Cst<I, S>)) {
+        // Visit the current node.
+        visit(self);
+
+        // Visit child nodes.
+        match self {
+            Cst::Token(_) => (),
+            Cst::Call(CallNode { head, body, src: _ }) => {
+                head.visit(visit);
+                body.as_op().visit_children(visit);
+            },
+            Cst::SyntaxError(SyntaxErrorNode {
+                err: _,
+                children,
+                src: _,
+            }) => {
+                children.visit(visit);
+            },
+            Cst::Prefix(PrefixNode(op)) => op.visit_children(visit),
+            Cst::Infix(InfixNode(op)) => op.visit_children(visit),
+            Cst::Postfix(PostfixNode(op)) => op.visit_children(visit),
+            Cst::Binary(BinaryNode(op)) => op.visit_children(visit),
+            Cst::Ternary(TernaryNode(op)) => op.visit_children(visit),
+            Cst::PrefixBinary(PrefixBinaryNode(op)) => op.visit_children(visit),
+            Cst::Compound(CompoundNode(op)) => op.visit_children(visit),
+            Cst::Group(GroupNode(op))
+            | Cst::GroupMissingCloser(GroupMissingCloserNode(op))
+            | Cst::GroupMissingOpener(GroupMissingOpenerNode(op)) => {
+                op.visit_children(visit)
+            },
+            Cst::Box(BoxNode {
+                kind: _,
+                children,
+                src: _,
+            }) => {
+                children.visit(visit);
+            },
+            // These node types have no child nodes.
+            Cst::Code(_) => (),
+        }
+    }
+
+    /// Mutably visit this node and every child node, recursively.
+    pub fn visit_mut(&mut self, visit: &mut dyn FnMut(&mut Cst<I, S>)) {
+        // Visit the current node.
+        visit(self);
+
+        // Visit child nodes.
+        match self {
+            Cst::Token(_) => (),
+            Cst::Call(CallNode { head, body, src: _ }) => {
+                head.visit_mut(visit);
+
+                body.as_op_mut().visit_children_mut(visit);
+            },
+            Cst::SyntaxError(SyntaxErrorNode {
+                err: _,
+                children,
+                src: _,
+            }) => children.visit_mut(visit),
+
+            Cst::Infix(InfixNode(op)) => {
+                op.visit_children_mut(visit);
+            },
+            Cst::Prefix(PrefixNode(op)) => {
+                op.visit_children_mut(visit);
+            },
+            Cst::Postfix(PostfixNode(op)) => {
+                op.visit_children_mut(visit);
+            },
+            Cst::Binary(BinaryNode(op)) => {
+                op.visit_children_mut(visit);
+            },
+            Cst::Ternary(TernaryNode(op)) => {
+                op.visit_children_mut(visit);
+            },
+            Cst::PrefixBinary(PrefixBinaryNode(op)) => {
+                op.visit_children_mut(visit)
+            },
+            Cst::Compound(CompoundNode(op)) => op.visit_children_mut(visit),
+            Cst::Group(GroupNode(op)) => op.visit_children_mut(visit),
+            Cst::GroupMissingCloser(GroupMissingCloserNode(op)) => {
+                op.visit_children_mut(visit)
+            },
+            Cst::GroupMissingOpener(GroupMissingOpenerNode(op)) => {
+                op.visit_children_mut(visit)
+            },
+
+            Cst::Box(BoxNode {
+                kind: _,
+                children,
+                src: _,
+            }) => {
+                children.visit_mut(visit);
+            },
+
+            // These node types have no child nodes.
+            Cst::Code(_) => (),
+        }
+    }
+
+    /// Transform this node tree by visiting this node and every child node, recursively.
+    pub fn map_visit(
+        self,
+        visit: &mut dyn FnMut(Cst<I, S>) -> Cst<I, S>,
+    ) -> Self {
+        // Visit the current node.
+        let self_ = visit(self);
+
+        // Visit child nodes.
+        let node: Cst<I, S> = match self_ {
+            Cst::Token(_) => return self_,
+            Cst::Call(CallNode { head, body, src }) => {
+                let head = head.map_visit(visit);
+
+                let body = body.map_op(|body_op: OperatorNode<_, _, _>| {
+                    body_op.map_visit(visit)
+                });
+
+                Cst::Call(CallNode { head, body, src })
+            },
+            Cst::SyntaxError(SyntaxErrorNode { err, children, src }) => {
+                let children = children.map_visit(visit);
+
+                Cst::SyntaxError(SyntaxErrorNode { err, children, src })
+            },
+
+            Cst::Infix(InfixNode(op)) => {
+                Cst::Infix(InfixNode(op.map_visit(visit)))
+            },
+            Cst::Prefix(PrefixNode(op)) => {
+                Cst::Prefix(PrefixNode(op.map_visit(visit)))
+            },
+            Cst::Postfix(PostfixNode(op)) => {
+                Cst::Postfix(PostfixNode(op.map_visit(visit)))
+            },
+            Cst::Binary(BinaryNode(op)) => {
+                Cst::Binary(BinaryNode(op.map_visit(visit)))
+            },
+            Cst::Ternary(TernaryNode(op)) => {
+                Cst::Ternary(TernaryNode(op.map_visit(visit)))
+            },
+            Cst::PrefixBinary(PrefixBinaryNode(op)) => {
+                Cst::PrefixBinary(PrefixBinaryNode(op.map_visit(visit)))
+            },
+            Cst::Compound(CompoundNode(op)) => {
+                Cst::Compound(CompoundNode(op.map_visit(visit)))
+            },
+            Cst::Group(GroupNode(op)) => {
+                Cst::Group(GroupNode(op.map_visit(visit)))
+            },
+            Cst::GroupMissingCloser(GroupMissingCloserNode(op)) => {
+                Cst::GroupMissingCloser(GroupMissingCloserNode(
+                    op.map_visit(visit),
+                ))
+            },
+            Cst::GroupMissingOpener(GroupMissingOpenerNode(op)) => {
+                Cst::GroupMissingOpener(GroupMissingOpenerNode(
+                    op.map_visit(visit),
+                ))
+            },
+
+            Cst::Box(BoxNode {
+                kind,
+                children,
+                src,
+            }) => {
+                let children = children.map_visit(visit);
+
+                Cst::Box(BoxNode {
+                    kind,
+                    children,
+                    src,
+                })
+            },
+
+            // These node types have no child nodes.
+            node @ Cst::Code(_) => node,
+        };
+
+        node
+    }
+}
+
+//======================================
+// OperatorNode
+//======================================
+
+impl<I, S, O> OperatorNode<I, S, O> {
+    /// Visit this node and every child node, recursively.
+    fn visit_children(&self, visit: &mut dyn FnMut(&Cst<I, S>)) {
+        let OperatorNode {
+            op: _,
+            children,
+            src: _,
+        } = self;
+
+        children.visit(visit);
+    }
+
+    /// Mutably visit this node and every child node, recursively.
+    fn visit_children_mut(&mut self, visit: &mut dyn FnMut(&mut Cst<I, S>)) {
+        let OperatorNode {
+            op: _,
+            children,
+            src: _,
+        } = self;
+
+        children.visit_mut(visit);
+    }
+
+    pub fn map_visit(
+        self,
+        visit: &mut dyn FnMut(Cst<I, S>) -> Cst<I, S>,
+    ) -> Self {
+        let OperatorNode { op, children, src } = self;
+
+        let children = children.map_visit(visit);
+
+        OperatorNode { op, children, src }
+    }
+}
+
+//======================================
+// CallHead
+//======================================
+
+impl<I, S> CallHead<I, S> {
+    /// Visit this node and every child node, recursively.
+    pub fn visit(&self, visit: &mut dyn FnMut(&Cst<I, S>)) {
+        match self {
+            CallHead::Concrete(head) => head.visit(visit),
+            CallHead::Aggregate(head) => head.visit(visit),
+        }
+    }
+
+    /// Mutably visit this node and every child node, recursively.
+    pub fn visit_mut(&mut self, visit: &mut dyn FnMut(&mut Cst<I, S>)) {
+        match self {
+            CallHead::Concrete(head) => head.visit_mut(visit),
+            CallHead::Aggregate(head) => head.visit_mut(visit),
+        }
+    }
+
+    pub fn map_visit(
+        self,
+        visit: &mut dyn FnMut(Cst<I, S>) -> Cst<I, S>,
+    ) -> Self {
+        match self {
+            CallHead::Concrete(head) => {
+                CallHead::Concrete(head.map_visit(visit))
+            },
+            CallHead::Aggregate(head) => {
+                let head: Cst<I, S> = *head;
+                CallHead::Aggregate(Box::new(head.map_visit(visit)))
+            },
+        }
+    }
+}
