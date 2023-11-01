1a90d33597c4cf537c76ac263c01d663ba127e1b
Connor Gray <connorg@wolfram.com>
2023-11-01 17:16:07 -0500

    cleanup: Combine parser_session.rs into parse.rs
    parser_session.rs shrunk considerably over the last couple
    of commits, and no longer makes sense to keep separate; there
    wasn't a clear conceptual reason for it to be separate
    for a while anyway.

--- crates/wolfram-parser/src/parse.rs
+++ crates/wolfram-parser/src/parse.rs
@@ -35,24 +35,26 @@
 pub(crate) mod parselet;
 pub(crate) mod operators;
 mod token_parselets;
-mod parser_session;
 
 
 use std::fmt::Debug;
 
 use crate::{
+    create_parse_result,
     cst::{
-        BinaryOperator, CompoundOperator, SyntaxErrorKind, TernaryOperator,
-        TriviaSeq,
+        BinaryOperator, CompoundOperator, Cst, CstSeq, SyntaxErrorKind,
+        TernaryOperator, TriviaSeq,
     },
-    panic_if_aborted,
-    // parselet::Parselet,
+    feature, panic_if_aborted,
+    parse::parselet::PrefixToplevelCloserParselet,
+    parse_cst::ParseCst,
     precedence::Precedence,
-
+    quirks,
     tokenize::{
         token_kind::Closer, tokenizer::Tokenizer_currentToken_stringifyAsFile,
-        TokenKind, TokenRef,
+        TokenKind, TokenRef, TokenStr, Tokenizer,
     },
+    NodeSeq, ParseOptions, ParseResult, QuirkSettings,
 };
 
 use self::{
@@ -64,7 +66,23 @@ use self::{
     token_parselets::{INFIX_PARSELETS, PREFIX_PARSELETS},
 };
 
-pub(crate) use self::parser_session::{ParserSession, TriviaSeqRef};
+//======================================
+// Types
+//======================================
+
+/// A parser session
+#[derive(Debug)]
+pub(crate) struct ParserSession<'i, B: ParseBuilder<'i> = ParseCst<'i>> {
+    pub(crate) tokenizer: Tokenizer<'i>,
+
+    #[cfg(not(test))]
+    pub(super) builder: B,
+
+    #[cfg(test)]
+    pub(crate) builder: B,
+
+    pub(crate) quirk_settings: QuirkSettings,
+}
 
 pub(crate) struct Context<'i> {
     continue_parse: Option<Box<dyn FnOnce(&mut ParserSession<'i>) + 'i>>,
@@ -75,6 +93,11 @@ pub(crate) struct Context<'i> {
 
     pub(crate) prec: Option<Precedence>,
 }
+
+//
+// Used mainly for collecting trivia that has been eaten
+//
+pub(crate) type TriviaSeqRef<'i> = TriviaSeq<TokenStr<'i>>;
 
 /// Handler for parse events to build up a representation of the parsed input.
 ///
@@ -319,6 +342,102 @@ impl TokenKind {
 }
 
 impl<'i> ParserSession<'i> {
+    pub fn new(input: &'i [u8], opts: &ParseOptions) -> ParserSession<'i> {
+        let ParseOptions {
+            first_line_behavior: _,
+            src_convention: _,
+            encoding_mode: _,
+            tab_width: _,
+            check_issues: _,
+            compute_oob: _,
+            quirk_settings,
+        } = *opts;
+
+        ParserSession {
+            tokenizer: Tokenizer::new(input, opts),
+            builder: ParseCst::new(),
+            quirk_settings,
+        }
+    }
+
+    /// Returns the complete input [`Buffer`][crate::source::Buffer].
+    pub fn input(&self) -> &'i [u8] {
+        self.tokenizer.input
+    }
+
+    pub fn concrete_parse_expressions(
+        &mut self,
+    ) -> ParseResult<CstSeq<TokenStr<'i>>> {
+        quirks::set_quirks(self.quirk_settings);
+
+        #[cfg(feature = "DIAGNOSTICS")]
+        {
+            DiagnosticsLog("enter parseExpressions");
+            DiagnosticsMarkTime();
+        }
+
+        //
+        // Collect all expressions
+        //
+
+        let mut exprs: CstSeq<TokenStr<'i>> = NodeSeq::new();
+
+        loop {
+            if feature::CHECK_ABORT && crate::abortQ() {
+                break;
+            }
+
+            let peek: TokenRef = self.tokenizer.peek_token();
+
+            if peek.tok == TokenKind::EndOfFile {
+                break;
+            }
+
+            if peek.tok.isTrivia() {
+                exprs.push(Cst::Token(peek));
+
+                peek.skip(&mut self.tokenizer);
+
+                continue;
+            }
+
+            //
+            // special top-level handling of stray closers
+            //
+            if peek.tok.isCloser() {
+                (PrefixToplevelCloserParselet {}).parse_prefix(self, peek);
+
+                exprs.push(self.builder.pop_finished_expr());
+
+                assert!(self.is_quiescent());
+
+                continue;
+            }
+
+            self.parse_prefix(peek);
+
+            exprs.push(self.builder.pop_finished_expr());
+
+            assert!(self.is_quiescent());
+        } // while (true)
+
+        #[cfg(feature = "DIAGNOSTICS")]
+        {
+            DiagnosticsLog("exit parseExpressions");
+            DiagnosticsLogTime();
+        }
+
+        if let Ok(input) = std::str::from_utf8(self.tokenizer.input) {
+            exprs = crate::error::reparse_unterminated(
+                exprs,
+                input,
+                usize::try_from(self.tokenizer.tab_width).unwrap(),
+            );
+        }
+
+        return create_parse_result(&self.tokenizer, exprs);
+    }
+
     /// Lookup and apply the [`PrefixParselet`] implementation associated
     /// with the [`TokenKind`] of `token`.
     // TODO(cleanup): Rename to avoid ambiguity with PrefixParselet::parse_prefix()?
@@ -665,6 +784,47 @@ impl<'i> ParserSession<'i> {
         assert!(self.tokenizer.GroupStack.is_empty());
 
         return true;
+    }
+
+    #[cfg(test)]
+    pub(crate) fn fatal_issues(&self) -> &Vec<crate::issue::Issue> {
+        &self.tokenizer.fatal_issues
+    }
+
+    #[cfg(test)]
+    pub(crate) fn non_fatal_issues(&self) -> &Vec<crate::issue::Issue> {
+        &self.tokenizer.non_fatal_issues
+    }
+}
+
+//======================================
+// TriviaSeq
+//======================================
+
+impl<'i> TriviaSeq<TokenStr<'i>> {
+    pub(crate) fn new() -> Self {
+        TriviaSeq(Vec::new())
+    }
+
+    pub(crate) fn reset(self, session: &mut Tokenizer) {
+        let TriviaSeq(vec) = self;
+
+        //
+        // Just need to reset the global buffer to the buffer of the first token in the sequence
+        //
+
+        if vec.is_empty() {
+            return;
+        }
+
+        let T = &vec[0];
+
+        T.reset(session);
+    }
+
+    pub(crate) fn push(&mut self, token: TokenRef<'i>) {
+        let TriviaSeq(vec) = self;
+        vec.push(token);
     }
 }
 
