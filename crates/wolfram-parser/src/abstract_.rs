e0828665447b151ebae4ee2c9222d96aeb5f1c96
Suman Banerjee <sumanb@wolfram.com>
2025-03-13 14:30:57 +0530

    Revert "docs: Improve flattenTimes() doc comment"
    This reverts commit 36cd7caf6d32e945388182f35b88c383b537102a.

--- crates/wolfram-parser/src/abstract_.rs
+++ crates/wolfram-parser/src/abstract_.rs
@@ -1894,10 +1894,7 @@ fn abstractPrefixPlus<I: TokenInput + Debug, S: TokenSource + Debug>(
 // Times handling
 //======================================
 
-/// abstract syntax of  `-a * b / c d \[InvisibleTimes] e \[Times] f`  is a single Times expression
-///
-/// The sequence returned from this function will be the arguments
-/// to a `Times[...]` expression.
+/// abstract syntax of  -a * b / c d \[InvisibleTimes] e \[Times] f  is a single Times expression
 fn flattenTimes<I: TokenInput + Debug, S: TokenSource + Debug>(
     nodes: Vec<Cst<I, S>>,
     data: S,
