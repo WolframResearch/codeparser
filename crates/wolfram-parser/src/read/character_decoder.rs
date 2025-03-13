a16ddd0f12edeb0f3c929dc057603bc7617b1400
Suman Banerjee <sumanb@wolfram.com>
2025-03-13 14:34:01 +0530

    Revert "cleanup: Remove unnecessary extra local variable `curSource`"
    This reverts commit 26927e168366f7e3118df5359f96434273628dd5.

--- crates/wolfram-parser/src/read/character_decoder.rs
+++ crates/wolfram-parser/src/read/character_decoder.rs
@@ -89,7 +89,9 @@ pub(super) fn CharacterDecoder_nextWLCharacter(
     session: &mut Reader,
     policy: NextPolicy,
 ) -> WLCharacter {
-    let mut point = session.next_source_char(policy);
+    let mut curSource = session.next_source_char(policy);
+
+    let mut point = curSource;
 
     if point != '\\' {
         incr_diagnostic!(CharacterDecoder_UnescapedCount);
@@ -109,16 +111,17 @@ pub(super) fn CharacterDecoder_nextWLCharacter(
 
     let escaped = session.mark();
 
-    point = session.peek_source_char(policy);
+    curSource = session.peek_source_char(policy);
 
-    let point_i32 = point.as_i32();
+    point = curSource;
 
-    if !(0x20 <= point_i32 && point_i32 <= 0x7e) {
+    if !(0x20 <= point.as_i32() && point.as_i32() <= 0x7e) {
         // MUSTTAIL
         return CharacterDecoder_handleUncommon(session, escaped, policy);
     }
 
-    let point_u8 = u8::try_from(point_i32).expect("unable to convert digit character to u8 value");
+    let point_u8 =
+        u8::try_from(point.as_i32()).expect("unable to convert digit character to u8 value");
 
     return CHARACTER_DECODER_HANDLER_TABLE[usize::from(point_u8)](session, escaped, policy);
 }
