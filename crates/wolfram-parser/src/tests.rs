cd885dded586dae0fecc81795d2b8cf1acf353a1
Suman Banerjee <sumanb@wolfram.com>
2025-03-13 14:34:38 +0530

    Revert "test: Add tests for binary Span with explicit and implicit first arg"
    This reverts commit 834cfa3445688fcbc756bd6ef998822dd38668df.

--- crates/wolfram-parser/src/tests.rs
+++ crates/wolfram-parser/src/tests.rs
@@ -27,7 +27,7 @@ use crate::{
     NodeSeq, ParseOptions, ParseResult, Tokens,
 };
 
-pub(crate) fn nodes(input: &str) -> Vec<Node<BorrowedTokenInput>> {
+fn nodes(input: &str) -> Vec<Node<BorrowedTokenInput>> {
     let mut session = ParserSession::new(input.as_bytes(), &ParseOptions::default());
 
     let result = session.concrete_parse_expressions();
@@ -37,7 +37,7 @@ pub(crate) fn nodes(input: &str) -> Vec<Node<BorrowedTokenInput>> {
     nodes
 }
 
-pub(crate) fn tokens(input: &str) -> Vec<Token<BorrowedTokenInput>> {
+fn tokens(input: &str) -> Vec<Token<BorrowedTokenInput>> {
     let mut session = ParserSession::new(input.as_bytes(), &ParseOptions::default());
 
     let tokens: Tokens<BorrowedTokenInput> = session.tokenize().unwrap();
