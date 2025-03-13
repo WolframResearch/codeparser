2815d2df0c8bd6ab71854adedc45a6183c61a164
Suman Banerjee <sumanb@wolfram.com>
2025-03-13 14:38:27 +0530

    Revert "cleanup: Address some dead code warnings"
    This reverts commit 005fd01dd52ba7a1bf174aab1ed1835e809def8e.

--- crate/src/node.rs
+++ crate/src/node.rs
@@ -389,9 +389,9 @@ impl Node {
             Node::MissingBecauseUnsafeCharacterEncoding(node) => node.check(),
             Node::SafeString(_) => true,
             Node::Call(node) => node.check(),
-            Node::SyntaxError(node) => node.check(),
-            Node::GroupMissingCloser(node) => node.check(),
-            Node::UnterminatedGroupNeedsReparse(node) => node.check(),
+            Node::SyntaxError(_) => todo!(),
+            Node::GroupMissingCloser(_) => todo!(),
+            Node::UnterminatedGroupNeedsReparse(_) => todo!(),
         }
     }
 }
@@ -729,7 +729,6 @@ impl CollectedIssuesNode {
 //======================================
 
 impl CollectedSourceLocationsNode {
-    #[allow(dead_code)]
     pub(crate) fn new(source_locs: HashSet<SourceLocation>) -> Self {
         CollectedSourceLocationsNode { source_locs }
     }
@@ -826,7 +825,6 @@ impl NodeContainer {
     //     Nodes.print(s);
     // }
 
-    #[allow(dead_code)]
     pub(crate) fn check(&self) -> bool {
         let NodeContainer { nodes } = self;
         return nodes.check();
