d63077d933a2b677861355c7b20211a1ce2a4773
Suman Banerjee <sumanb@wolfram.com>
2025-03-13 14:36:56 +0530

    Revert "cleanup: Use standard naming convention for top-level functions + add docs table"
    This reverts commit 3147ae80b7f21660d0fe2bd531ddaec8c95ffabf.

--- crates/wolfram-parser/src/lib.rs
+++ crates/wolfram-parser/src/lib.rs
@@ -1,20 +1,3 @@
-//! Wolfram Language input form parser.
-//!
-//! This library implements a fully-featured parser for [Wolfram Language][WL]
-//! input form syntax. Given a string containing Wolfram Language code, either
-//! an Abstract Syntax Tree (AST) or Concrete Syntax Tree (CST) can be parsed.
-//!
-//! [WL]: https://wolfram.com/language
-//!
-//!
-//! # API
-//!
-//! Input   | Tokenize             | Parse concrete syntax    | Parse abstract syntax
-//! --------|----------------------|--------------------------|--------------------
-//! `&str`  | [`tokenize()`]       | [`parse_to_cst()`]       | [`parse_to_ast()`]
-//! `&[u8]` | [`tokenize_bytes()`] | [`parse_bytes_to_cst()`] | [`parse_bytes_to_ast()`]
-//!
-
 //
 // Lints
 //
@@ -364,7 +347,7 @@ impl ParseOptions {
 
 use crate::parser_session::ParserSession;
 
-/// Parse a string containing Wolfram Language input into a sequence of tokens.
+/// Parse bytes containing Wolfram Language input into a sequence of tokens.
 ///
 /// # Examples
 ///
@@ -372,11 +355,12 @@ use crate::parser_session::ParserSession;
 ///
 /// ```
 /// use wolfram_parser::{
-///     tokenize, ParseOptions, Tokens,
+///     tokenize_bytes, ParseOptions, Tokens,
 ///     test_utils::{token, src}
 /// };
 ///
-/// let Tokens(tokens) = tokenize("2 + 2", &ParseOptions::default());
+/// let Tokens(tokens) = tokenize_bytes(b"2 + 2", &ParseOptions::default())
+///     .unwrap();
 ///
 /// assert_eq!(tokens, &[
 ///     token![Integer, "2" @ 0, src!(1:1-1:2)],
@@ -386,12 +370,6 @@ use crate::parser_session::ParserSession;
 ///     token![Integer, "2" @ 4, src!(1:5-1:6)],
 /// ]);
 /// ```
-pub fn tokenize<'i>(input: &'i str, opts: &ParseOptions) -> Tokens<BorrowedTokenInput<'i>> {
-    tokenize_bytes(input.as_bytes(), opts)
-        .expect("unexpected character encoding error tokenizing &str")
-}
-
-/// Parse bytes containing Wolfram Language input into a sequence of tokens.
 pub fn tokenize_bytes<'i>(
     input: &'i [u8],
     opts: &ParseOptions,
@@ -420,28 +398,27 @@ pub fn tokenize_bytes<'i>(
 // Parse CST
 //======================================
 
-/// Parse a string containing Wolfram Language input into a concrete syntax tree.
+/// Parse a string containing Wolfram Language input into concrete syntax tree.
 ///
 /// # Examples
 ///
 /// Parse `2 + 2`:
 ///
 /// ```
-/// use wolfram_parser::{parse_to_cst, ParseOptions};
+/// use wolfram_parser::{parse_concrete, ParseOptions};
 ///
-/// let result = parse_to_cst("2 + 2", &ParseOptions::default());
+/// let result = parse_concrete("2 + 2", &ParseOptions::default());
 ///
 /// // TODO: assert_eq!(result.nodes(), &[]);
 /// ```
-pub fn parse_to_cst<'i>(
+pub fn parse_concrete<'i>(
     input: &'i str,
     opts: &ParseOptions,
 ) -> ParseResult<CstNode<BorrowedTokenInput<'i>>> {
-    parse_bytes_to_cst(input.as_bytes(), opts)
+    parse_concrete_bytes(input.as_bytes(), opts)
 }
 
-/// Parse bytes containing Wolfram Language input into a concrete syntax tree.
-pub fn parse_bytes_to_cst<'i>(
+pub fn parse_concrete_bytes<'i>(
     bytes: &'i [u8],
     opts: &ParseOptions,
 ) -> ParseResult<CstNode<BorrowedTokenInput<'i>>> {
@@ -470,12 +447,12 @@ pub fn parse_bytes_to_cst<'i>(
 //======================================
 
 /// Parse a string containing Wolfram Language input into an abstract syntax tree.
-pub fn parse_to_ast<'i>(input: &'i str, opts: &ParseOptions) -> ParseResult<AstNode> {
-    parse_bytes_to_ast(input.as_bytes(), opts)
+pub fn parse_ast<'i>(input: &'i str, opts: &ParseOptions) -> ParseResult<AstNode> {
+    parse_ast_bytes(input.as_bytes(), opts)
 }
 
 /// Parse bytes containing Wolfram Language input into an abstract syntax tree.
-pub fn parse_bytes_to_ast<'i>(bytes: &'i [u8], opts: &ParseOptions) -> ParseResult<AstNode> {
+pub fn parse_ast_bytes<'i>(bytes: &'i [u8], opts: &ParseOptions) -> ParseResult<AstNode> {
     let ParseOptions {
         first_line_behavior,
         src_convention,
