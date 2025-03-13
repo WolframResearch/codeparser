c2b94ec35c53a59f10d8d8b6a5aaed22a9782bbc
Suman Banerjee <sumanb@wolfram.com>
2025-03-13 14:30:49 +0530

    Revert "polish: Support `--flatten-times` option to codeparser exe"
    This reverts commit 814223feaf4515f8af7a6c7ac0aeba0bfa9e89d0.

--- crates/wolfram-parser/src/bin/main.rs
+++ crates/wolfram-parser/src/bin/main.rs
@@ -1,8 +1,6 @@
 use std::io::{self, Write};
 
-use wolfram_parser::{
-    fmt_as_expr::FmtAsExpr, ParseOptions, QuirkSettings, StringifyMode,
-};
+use wolfram_parser::{fmt_as_expr::FmtAsExpr, ParseOptions, StringifyMode};
 
 
 // #if DIAGNOSTICS
@@ -31,7 +29,6 @@ fn main() {
     let mut file_input = None;
     let mut api_mode = ApiMode::CstExpr;
     let mut output_mode = OutputMode::Print;
-    let mut quirks = QuirkSettings::default();
 
     let args: Vec<String> = std::env::args().skip(1).collect();
 
@@ -56,26 +53,21 @@ fn main() {
             "-check" | "-syntaxq" | "-syntaxQ" => {
                 output_mode = OutputMode::SyntaxQ;
             },
-            "--flatten-times" => {
-                quirks.flatten_times = true;
-            },
-            _ => panic!("unrecognized argument: {arg}"),
+            _ => panic!(),
         }
 
         i += 1;
     }
 
     let result = match file_input {
-        Some(file_input) => {
-            read_file(&file_input, api_mode, output_mode, quirks)
-        },
-        None => read_std_in(api_mode, output_mode, quirks),
+        Some(file_input) => read_file(&file_input, api_mode, output_mode),
+        None => read_std_in(api_mode, output_mode),
     };
 
     return result;
 }
 
-fn read_std_in(mode: ApiMode, output_mode: OutputMode, quirks: QuirkSettings) {
+fn read_std_in(mode: ApiMode, output_mode: OutputMode) {
     loop {
         let mut input = String::new();
 
@@ -84,7 +76,7 @@ fn read_std_in(mode: ApiMode, output_mode: OutputMode, quirks: QuirkSettings) {
 
         io::stdin().read_line(&mut input).unwrap();
 
-        handle(input.trim_end().as_bytes(), mode, output_mode, quirks)
+        handle(input.trim_end().as_bytes(), mode, output_mode)
     }
 
     // #if DIAGNOSTICS
@@ -92,53 +84,50 @@ fn read_std_in(mode: ApiMode, output_mode: OutputMode, quirks: QuirkSettings) {
     // #endif // DIAGNOSTICS
 }
 
-fn read_file(
-    file: &str,
-    mode: ApiMode,
-    output_mode: OutputMode,
-    quirks: QuirkSettings,
-) {
+fn read_file(file: &str, mode: ApiMode, output_mode: OutputMode) {
     let fb: Vec<u8> = std::fs::read(file).expect("error reading file");
 
-    handle(fb.as_slice(), mode, output_mode, quirks)
+    handle(fb.as_slice(), mode, output_mode)
 
     // #if DIAGNOSTICS
     //     DiagnosticsPrint();
     // #endif // DIAGNOSTICS
 }
 
-fn handle(
-    input: &[u8],
-    mode: ApiMode,
-    output_mode: OutputMode,
-    quirks: QuirkSettings,
-) {
-    let mut opts = ParseOptions::default();
-    opts.quirk_settings = quirks;
-
+fn handle(input: &[u8], mode: ApiMode, output_mode: OutputMode) {
     match mode {
         ApiMode::Tokenize => {
-            let result = wolfram_parser::tokenize_bytes(input, &opts).unwrap();
+            let result =
+                wolfram_parser::tokenize_bytes(input, &ParseOptions::default())
+                    .unwrap();
             output(output_mode, FmtAsExpr(&result));
         },
         ApiMode::Leaf => {
             let result = wolfram_parser::parse_to_token(
                 input,
-                &opts,
+                &ParseOptions::default(),
                 StringifyMode::Normal,
             );
             output(output_mode, FmtAsExpr(result.node_seq()));
         },
         ApiMode::SafeString => {
-            let result = wolfram_parser::safe_string(input, &opts).unwrap();
+            let result =
+                wolfram_parser::safe_string(input, &ParseOptions::default())
+                    .unwrap();
             output(output_mode, result);
         },
         ApiMode::CstExpr => {
-            let result = wolfram_parser::parse_bytes_cst(input, &opts);
+            let result = wolfram_parser::parse_bytes_cst(
+                input,
+                &ParseOptions::default(),
+            );
             output(output_mode, FmtAsExpr(result.node_seq()));
         },
         ApiMode::Ast => {
-            let result = wolfram_parser::parse_bytes_ast(input, &opts);
+            let result = wolfram_parser::parse_bytes_ast(
+                input,
+                &ParseOptions::default(),
+            );
             output(output_mode, format!("{:#?}", result.node_seq()));
         },
     }
