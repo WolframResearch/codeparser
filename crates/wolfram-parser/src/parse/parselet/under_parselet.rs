c15dabaebea05849b3ef2fc787b73e558a5c0799
Suman Banerjee <sumanb@wolfram.com>
2025-03-13 14:30:17 +0530

    Revert "test: Test Error_ExpectedLetterlike during _ (Blank) context-sensitive parsing"
    This reverts commit 0b50b466f06dbe52f5527579a8191fedc1c274a6.

--- crates/wolfram-parser/src/parse/parselet/under_parselet.rs
+++ crates/wolfram-parser/src/parse/parselet/under_parselet.rs
@@ -57,7 +57,7 @@ impl PrefixParselet for UnderParselet {
 
         if tok.tok == TokenKind::Error_ExpectedLetterlike {
             //
-            // Something like  _a`  (TID:231016/1)
+            // Something like  _a`
             //
             // It's nice to include the error inside of the blank
             //
@@ -115,7 +115,7 @@ impl UnderParselet {
 
         if tok.tok == TokenKind::Error_ExpectedLetterlike {
             //
-            // Something like  a_b`  (TID:231016/2)
+            // Something like  a_b`
             //
             // It's nice to include the error inside of the blank
             //
