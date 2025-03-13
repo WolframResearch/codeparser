5733a1efec9a12f5c1687e3cb978fc474445edb5
Suman Banerjee <sumanb@wolfram.com>
2025-03-13 14:30:44 +0530

    Revert "devex: Improve formatting of Ast and AstMetadata"
    This reverts commit d8acdef07f66b989a8d6ab2da29fde2054f3a7ac.

--- crates/wolfram-parser/src/tokenize/token.rs
+++ crates/wolfram-parser/src/tokenize/token.rs
@@ -1,4 +1,4 @@
-use std::fmt::{self, Debug, Display};
+use std::fmt::{self, Debug};
 
 use crate::{
     source::{BufferAndLength, Source, Span},
@@ -492,23 +492,10 @@ impl Debug for TokenString {
         match std::str::from_utf8(buf) {
             Ok(str) => {
                 if cfg!(test) {
-                    write!(f, "{self}")
+                    write!(f, "TokenString::new({str:?})")
                 } else {
                     f.debug_struct("TokenString").field("buf", &str).finish()
                 }
-            },
-            Err(_) => f.debug_struct("TokenString").field("buf", buf).finish(),
-        }
-    }
-}
-
-impl Display for TokenString {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let TokenString { buf } = self;
-
-        match std::str::from_utf8(buf) {
-            Ok(str) => {
-                write!(f, "{str:?}")
             },
             Err(_) => f.debug_struct("TokenString").field("buf", buf).finish(),
         }
