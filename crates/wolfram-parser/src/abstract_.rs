00b04465150d6df06cc86998e757f0b23c1e18a2
Suman Banerjee <sumanb@wolfram.com>
2025-03-13 14:29:27 +0530

    Revert "cleanup: Factor out common `-1 * ...` fake tokens to Negated::into_cst()"
    This reverts commit 03d45248ba9dc6fff42b6f45f7922c6c024838b3.

--- crates/wolfram-parser/src/abstract_.rs
+++ crates/wolfram-parser/src/abstract_.rs
@@ -1600,7 +1600,7 @@ enum Negated<I, S> {
     InfixTimesSeq(NodeSeq<Cst<I, S>>),
 }
 
-impl<I: TokenInput, S: TokenSource> Negated<I, S> {
+impl<I: TokenInput, S> Negated<I, S> {
     fn into_cst(self, data: S) -> Cst<TokenString, S> {
         match self {
             Negated::Integer0 => {
@@ -1615,15 +1615,10 @@ impl<I: TokenInput, S: TokenSource> Negated<I, S> {
 
                 agg::WL!( LeafNode[Real, format!("-{str}"), data] )
             },
-            Negated::InfixTimesSeq(NodeSeq(children)) => {
-                let children = join(
-                    [agg::WL!(ToNode[-1]), agg::WL!(LeafNode[Star, "*", <||>])],
-                    children,
-                );
-
+            Negated::InfixTimesSeq(children) => {
                 let infix = InfixNode(OperatorNode {
                     op: InfixOperator::Times,
-                    children: NodeSeq(children).into_owned_input(),
+                    children: children.into_owned_input(),
                     src: data,
                 });
 
@@ -1717,6 +1712,11 @@ fn negate<I: TokenInput + Debug, S: TokenSource + Debug>(
         src: _,
     })) = node.clone()
     {
+        let children = join(
+            [agg::WL!(ToNode[-1]), agg::WL!(LeafNode[Star, "*", <||>])],
+            children,
+        );
+
         return Negated::InfixTimesSeq(NodeSeq(children));
     }
 
@@ -1724,7 +1724,11 @@ fn negate<I: TokenInput + Debug, S: TokenSource + Debug>(
     // Otherwise, returns a Times[-1, node] expression
     //------------------------------------------------
 
-    let children = NodeSeq(vec![node]);
+    let children = NodeSeq(vec![
+        agg::WL!(ToNode[-1]),
+        agg::WL!(LeafNode[Star, "*", <||>]),
+        node,
+    ]);
 
     Negated::InfixTimesSeq(children)
 }
