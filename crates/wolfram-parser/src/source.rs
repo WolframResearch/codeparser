c22a2c083c3dcad6bfe1f89104fb0f5272d40d1a
Suman Banerjee <sumanb@wolfram.com>
2025-03-13 14:33:25 +0530

    Revert "cleanup: Simplify duplicated logic in Display for SourceCharacter"
    This reverts commit 4395ca4eb64f66b6e197e7cdec8ad350ad6d039f.

--- crates/wolfram-parser/src/source.rs
+++ crates/wolfram-parser/src/source.rs
@@ -1170,31 +1170,65 @@ impl Display for SourceCharacter {
 
         let val: char = val;
 
-        let escape: Option<Escape> =
-            if val > '\u{7f}' && codepoint_has_longname(val) {
+        if val > '\u{ffff}' {
+            if codepoint_has_longname(val) {
                 //
-                // Use LongName if available and not ASCII
+                // Use LongName if available
                 //
-                Some(Escape::LongName)
-            } else if val > '\u{ffff}' {
-                Some(Escape::Hex6)
-            } else if val > '\u{ff}' {
-                Some(Escape::Hex4)
-            } else if val > '\u{7f}' {
-                Some(Escape::Hex2)
-            } else {
-                None
-            };
 
-        if let Some(escape) = escape {
+                return write!(
+                    stream,
+                    "{:#}",
+                    WLCharacter::new_with_escape(val, Escape::LongName)
+                );
+            }
+
             return write!(
                 stream,
                 "{:#}",
-                WLCharacter::new_with_escape(val, escape)
+                WLCharacter::new_with_escape(val, Escape::Hex6)
             );
         }
 
-        debug_assert!(val.is_ascii());
+        if val > '\u{ff}' {
+            if codepoint_has_longname(val) {
+                //
+                // Use LongName if available
+                //
+
+                return write!(
+                    stream,
+                    "{:#}",
+                    WLCharacter::new_with_escape(val, Escape::LongName)
+                );
+            }
+
+            return write!(
+                stream,
+                "{:#}",
+                WLCharacter::new_with_escape(val, Escape::Hex4)
+            );
+        }
+
+        if val > '\u{7f}' {
+            if codepoint_has_longname(val) {
+                //
+                // Use LongName if available
+                //
+
+                return write!(
+                    stream,
+                    "{:#}",
+                    WLCharacter::new_with_escape(val, Escape::LongName)
+                );
+            }
+
+            return write!(
+                stream,
+                "{:#}",
+                WLCharacter::new_with_escape(val, Escape::Hex2)
+            );
+        }
 
         //
         // ASCII is untouched
