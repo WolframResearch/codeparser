47ae183310e2bf7c61fa4c5027392730edffdf30
Suman Banerjee <sumanb@wolfram.com>
2025-03-13 14:36:38 +0530

    Revert "cleanup: Delete old commented PrintTo definitions"
    This reverts commit 9c40377f9ca52d4585f4f23fee48a15e289f603a.

--- crates/wolfram-parser/src/token.rs
+++ crates/wolfram-parser/src/token.rs
@@ -184,6 +184,12 @@ fn test_token_size() {
     assert_eq!(std::mem::size_of::<BorrowedTokenInput>(), 24);
 }
 
+//
+// For googletest
+//
+#[cfg(feature = "BUILD_TESTS")]
+fn PrintTo(token: &Token, stream: &mut std::ostream);
+
 impl<'i> TokenRef<'i> {
     // pub(crate) fn new(tok: TokenKind, buf: BufferAndLength, src: Source) -> Self {
     pub(crate) fn new(tok: TokenKind, buf: BufferAndLength<'i>, src: Source) -> Self {
@@ -380,6 +386,14 @@ impl<I, S> Token<I, S> {
     //     Src.print(s);
     //     s << "]";
     // }
+
+    //
+    // For googletest
+    //
+    #[cfg(feature = "BUILD_TESTS")]
+    fn PrintTo(T: &Token, s: &mut std::ostream) {
+        T.print(*s);
+    }
 }
 
 impl PartialEq<Token> for Token<BorrowedTokenInput<'_>> {
