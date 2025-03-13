79f2213ba5bd354e36c7eeec58c8a6e68ed72ffa
Suman Banerjee <sumanb@wolfram.com>
2025-03-13 14:28:30 +0530

    Revert "cleanup: Manually inline ParserSession::reparse_unterminated() into its only callsite"
    This reverts commit 5525d5c6e652a1aec07af87ed7d9bfb965e3f3f9.

--- crates/wolfram-parser/src/parse/parser_session.rs
+++ crates/wolfram-parser/src/parse/parser_session.rs
@@ -128,15 +128,24 @@ impl<'i> ParserSession<'i> {
             DiagnosticsLogTime();
         }
 
+        let exprs = self.reparse_unterminated(exprs);
+
+        return create_parse_result(&self.tokenizer, exprs);
+    }
+
+    fn reparse_unterminated(
+        &self,
+        mut nodes: CstSeq<TokenStr<'i>>,
+    ) -> CstSeq<TokenStr<'i>> {
         if let Ok(input) = std::str::from_utf8(self.tokenizer.input) {
-            exprs = crate::error::reparse_unterminated(
-                exprs,
+            nodes = crate::error::reparse_unterminated(
+                nodes,
                 input,
                 usize::try_from(self.tokenizer.tab_width).unwrap(),
             );
         }
 
-        return create_parse_result(&self.tokenizer, exprs);
+        nodes
     }
 
     #[cfg(test)]
