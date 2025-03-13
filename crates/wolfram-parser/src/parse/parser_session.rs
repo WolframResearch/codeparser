01bd0431aaedb23c1991a3c4086e8b0a24f761d8
Suman Banerjee <sumanb@wolfram.com>
2025-03-13 14:30:24 +0530

    Revert "cleanup: Make TriviaSeq into a tuple struct"
    This reverts commit 109d133855ccf986c36b17f8aefd47f9acbcb09e.

--- crates/wolfram-parser/src/parse/parser_session.rs
+++ crates/wolfram-parser/src/parse/parser_session.rs
@@ -49,7 +49,9 @@ pub(crate) type NodeStack<'i> = Vec<Cst<TokenStr<'i>>>;
 // Used mainly for collecting trivia that has been eaten
 //
 #[derive(Debug)]
-pub(crate) struct TriviaSeq<'i>(pub(crate) Vec<Token<TokenStr<'i>>>);
+pub(crate) struct TriviaSeq<'i> {
+    pub(crate) vec: Vec<Token<TokenStr<'i>>>,
+}
 
 pub struct ParseResult<T> {
     /// Tokens, concrete syntax, or abstract syntax.
@@ -395,11 +397,11 @@ impl<'i> ParserSession<'i> {
 
 impl<'i> TriviaSeq<'i> {
     pub(crate) fn new() -> Self {
-        TriviaSeq(Vec::new())
+        TriviaSeq { vec: Vec::new() }
     }
 
     pub fn reset(self, session: &mut Tokenizer) {
-        let TriviaSeq(vec) = self;
+        let TriviaSeq { vec } = self;
 
         //
         // Just need to reset the global buffer to the buffer of the first token in the sequence
@@ -415,8 +417,7 @@ impl<'i> TriviaSeq<'i> {
     }
 
     pub fn push(&mut self, token: TokenRef<'i>) {
-        let TriviaSeq(vec) = self;
-        vec.push(token);
+        self.vec.push(token);
     }
 }
 
