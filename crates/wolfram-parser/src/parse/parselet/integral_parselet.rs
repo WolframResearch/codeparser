6c1721435d3e5963c880ffe70ff2c02c7c9bbc4f
Suman Banerjee <sumanb@wolfram.com>
2025-03-13 14:27:50 +0530

    Revert "cleanup: Manually inline reduceIntegrate() and reduceIntegral() into their only call sites"
    This reverts commit d99894dc79fe6a26a9fc11bba9e7e8f06f630e4b.

--- crates/wolfram-parser/src/parse/parselet/integral_parselet.rs
+++ crates/wolfram-parser/src/parse/parselet/integral_parselet.rs
@@ -73,10 +73,8 @@ impl IntegralParselet {
             // TID:231113/3: "\[Integral] f"
             //
 
-            session.reduce_prefix(self.Op2);
-
             // MUSTTAIL
-            return session.parse_climb();
+            return IntegralParselet::reduceIntegral(self, session);
         }
 
         session.push_trivia_seq(trivia1);
@@ -84,13 +82,29 @@ impl IntegralParselet {
         let self_ = *self;
         let ctxt = session.top_context();
         ctxt.set_callback_with_state(move |session| {
-            session.reduce_prefix_binary(self_.Op1);
-
-            session.parse_climb();
+            IntegralParselet::reduceIntegrate(&self_, session)
         });
 
         // MUSTTAIL
         return session.parse_prefix(tok);
+    }
+
+    fn reduceIntegrate<'i, B: ParseBuilder<'i> + 'i>(
+        &self,
+        session: &mut ParserSession<'i, B>,
+    ) {
+        session.reduce_prefix_binary(self.Op1);
+
+        session.parse_climb();
+    }
+
+    fn reduceIntegral<'i, B: ParseBuilder<'i> + 'i>(
+        &self,
+        session: &mut ParserSession<'i, B>,
+    ) {
+        session.reduce_prefix(self.Op2);
+
+        session.parse_climb();
     }
 }
 
