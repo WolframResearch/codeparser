3a6ab8676715c77c8709591a97914a014fc52dfe
Suman Banerjee <sumanb@wolfram.com>
2025-03-13 14:31:12 +0530

    Revert "cleanup: Make processInfixBinaryAtQuirk(..) return Ast"
    This reverts commit 55f46e55cbeb71046a59a50953da1195a6083aa1.

--- crates/wolfram-parser/src/abstract_.rs
+++ crates/wolfram-parser/src/abstract_.rs
@@ -757,6 +757,7 @@ pub fn abstract_<I: TokenInput + Debug, S: TokenSource + Debug>(
                         })
                         // make sure to reverse children of Divisible
                         .rev()
+                        .map(abstract_)
                         .collect();
 
                     WL!( CallNode[ToNode[Divisible], processed, data] )
@@ -825,6 +826,7 @@ pub fn abstract_<I: TokenInput + Debug, S: TokenSource + Debug>(
                                 op.to_symbol().symbol_name().as_str(),
                             )
                         })
+                        .map(abstract_)
                         .collect();
 
                     WL!( CallNode[ToNode(op), children, data] )
@@ -1842,9 +1844,10 @@ fn abstractPlus<I: TokenInput + Debug, S: TokenSource + Debug>(
         children.into_iter().map(flattenPrefixPlus)
     };
 
-    let children = flattened
-        .map(|node| processInfixBinaryAtQuirk(node, "Plus"))
-        .collect();
+    let processed =
+        flattened.map(|node| processInfixBinaryAtQuirk(node, "Plus"));
+
+    let children = processed.map(abstract_).collect();
 
     WL!( CallNode[ToNode[Plus], children, data])
 }
@@ -1982,10 +1985,12 @@ fn abstractTimes_InfixNode<I: TokenInput + Debug, S: TokenSource + Debug>(
 
     let flattened = flattenTimes(children, data.clone());
 
-    let children: Vec<Ast> = flattened
+    let processed: Vec<Cst<TokenString, S>> = flattened
         .into_iter()
         .map(|node| processInfixBinaryAtQuirk(node, "Times"))
         .collect();
+
+    let children = processed.into_iter().map(abstract_).collect();
 
     WL!( CallNode[ToNode[Times], children, data] )
 }
@@ -2014,7 +2019,7 @@ pub(crate) fn processInfixBinaryAtQuirk<
 >(
     node: Cst<I, S>,
     symName: &str,
-) -> Ast {
+) -> Cst<I, S> {
     match node {
         Cst::Binary(BinaryNode(OperatorNode {
             op: BinaryOperator::CodeParser_BinaryAt,
@@ -2031,7 +2036,7 @@ pub(crate) fn processInfixBinaryAtQuirk<
                     ..
                 }) if input.as_str() == symName
             ) {
-                return abstract_(node);
+                return node;
             }
 
             if !matches!(middle, Cst::Token(Token { tok: TK::At, .. })) {
@@ -2058,9 +2063,9 @@ pub(crate) fn processInfixBinaryAtQuirk<
                 rhs[[3]] = data;
             */
 
-            abstract_(rhs)
+            rhs
         },
-        _ => abstract_(node),
+        _ => node,
     }
 }
 
