4df28d209f21d95d392cdca60987d67640e60394
Suman Banerjee <sumanb@wolfram.com>
2025-03-13 14:29:22 +0530

    Revert "cleanup: Remove agg::WL! and TokenSource::unknown()"
    This reverts commit d99b2a7c4a738cff4c78d584b89d0fa992f3db60.

--- crates/wolfram-parser/src/ast.rs
+++ crates/wolfram-parser/src/ast.rs
@@ -336,7 +336,7 @@ macro_rules! WL {
     //========================
 
     (CallNode[ToNode[$token_kind:ident], { $($args:expr),* }, <||>]) => {
-        WL!( CallNode[ToNode[$token_kind], { $($args),* }, $crate::ast::AstMetadata::empty() ])
+        WL!( CallNode[ToNode[$token_kind], { $($args),* }, S::unknown()])
     };
     (CallNode[ToNode[$token_kind:ident], { $($args:expr),* }, $data:expr]) => {{
         $crate::ast::Ast::Call {
@@ -362,7 +362,7 @@ macro_rules! WL {
     }};
 
     (CallNode[$head:expr, { $($args:expr),* }, <||>]) => {{
-        WL!( CallNode[$head, { $($args),* }, $crate::ast::AstMetadata::empty()] )
+        WL!( CallNode[$head, { $($args),* }, S::unknown()] )
     }};
     (CallNode[$head:expr, { $($args:expr),* }, $data:expr]) => {{
         $crate::ast::Ast::Call {
