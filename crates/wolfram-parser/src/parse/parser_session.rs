23befe1abad5396af4b70c1ababd6363419a1e4c
Suman Banerjee <sumanb@wolfram.com>
2025-03-13 14:29:17 +0530

    Revert "cleanup: Rename fatalIssues/nonFatalIssues fields to snake_case"
    This reverts commit df67c8da879b76be95a1156776b2db278a60d869.

--- crates/wolfram-parser/src/parse/parser_session.rs
+++ crates/wolfram-parser/src/parse/parser_session.rs
@@ -146,13 +146,13 @@ impl<'i> ParserSession<'i> {
     }
 
     #[cfg(test)]
-    pub(crate) fn fatal_issues(&self) -> &Vec<crate::issue::Issue> {
-        &self.tokenizer.fatal_issues
+    pub(crate) fn fatalIssues(&self) -> &Vec<crate::issue::Issue> {
+        &self.tokenizer.fatalIssues
     }
 
     #[cfg(test)]
-    pub(crate) fn non_fatal_issues(&self) -> &Vec<crate::issue::Issue> {
-        &self.tokenizer.non_fatal_issues
+    pub(crate) fn nonFatalIssues(&self) -> &Vec<crate::issue::Issue> {
+        &self.tokenizer.nonFatalIssues
     }
 }
 
