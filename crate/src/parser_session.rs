d1f9e21538f3121b042e2c684e3e1024f5b56d62
Suman Banerjee <sumanb@wolfram.com>
2025-03-13 14:38:13 +0530

    Revert "cleanup: Remove ParserSession::abortQ(), use crate::abortQ()"
    This reverts commit 204a3b7b7992f9cea2689a3327a931fffaa00070.

--- crate/src/parser_session.rs
+++ crate/src/parser_session.rs
@@ -121,7 +121,7 @@ impl<'i> ParserSession<'i> {
         let mut exprs = NodeSeq::new();
 
         loop {
-            if feature::CHECK_ABORT && crate::abortQ() {
+            if feature::CHECK_ABORT && self.abortQ() {
                 break;
             }
 
@@ -174,7 +174,7 @@ impl<'i> ParserSession<'i> {
         let mut nodes = NodeSeq::new();
 
         loop {
-            if feature::CHECK_ABORT && crate::abortQ() {
+            if feature::CHECK_ABORT && self.abortQ() {
                 break;
             }
 
@@ -273,6 +273,11 @@ impl<'i> ParserSession<'i> {
         };
 
         result
+    }
+
+    // TODO(cleanup): This doesn't need to be a method on ParserSession.
+    pub(crate) fn abortQ(&self) -> bool {
+        crate::abortQ()
     }
 
     pub(crate) fn fatalIssues(&self) -> &IssuePtrSet {
